227111e2917bcd13de362f93ce66ae49befc61aa
Kirik snegovick Konstantin <snegovick@gmail.com>
2014-06-01 20:32:14 +0400

    added response parsing

--- src/parser.c
+++ src/parser.c
@@ -1,9 +1,23 @@
 #include "parser.h"
 #include <stdio.h>
 
-void process(const void * data, unsigned int size) {
+void process_response(const void * data, unsigned int size, struct response *rs) {
   int i = 0;
   char * data_ptr = (char*)data;
+  printf("function: %i\r\n", rs->header.function);
+  /* switch (rs->header.function) { */
+    
+  /* } */
+  printf("\r\nprocess data: ");
+  for (;i<size;i++) {
+    printf("%c", data_ptr[i]);
+  }
+}
+
+void process_query(const void * data, unsigned int size, struct query *qs) {
+  int i = 0;
+  char * data_ptr = (char*)data;
+  printf("function: %i\r\n", qs->header.function);
   printf("\r\nprocess data: ");
   for (;i<size;i++) {
     printf("%c", data_ptr[i]);
@@ -11,10 +25,10 @@ void process(const void * data, unsigned int size) {
 }
 
 int main (void) {
-  char *buf = ":0603006Baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0003AB\r\ngjksdfg:0603006b0003AB\r\nsdghsdf";
+  char *buf = ":0603006Baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0003AB\r\ngjksdfg:0603006b0003AB\r\nsdghsdfdfghdfhsfh:110105cd6bb20e1b00\r\nsdfglbhawset";
   uint8_t storage[32];
   struct emb e;
-  emb_init(&e, storage, 32, process);
+  emb_init(&e, storage, 32, process_response, process_query, 1);
   int i = 1;
   char *c = buf;
   while (1) {
--- src/parser.h
+++ src/parser.h
@@ -8,26 +8,176 @@
 #define COND_N 2
 
 #define STATIC_BUFFER_SIZE 128
+#define BINARY_BUFFER_SIZE 128
+
+
+#define F_READ_COIL_STATUS          1
+#define F_READ_INPUT_STATUS         2
+#define F_READ_HOLDING_REGISTERS    3
+#define F_READ_INPUT_REGISTERS      4
+#define F_FORCE_SINGLE_COIL         5
+#define F_PRESET_SINGLE_REGISTER    6
+#define F_READ_EXCEPTION_STATUS     7
+#define F_DIAGNOSTICS               8
+#define F_FORCE_MULTIPLE_COILS      15
+#define F_PRESET_MULTIPLE_REGISTERS 16
+#define F_REPORT_SLAVE_ID           17
+
+struct query_header {
+  uint8_t header;
+  uint8_t slave_address;
+  uint8_t function;
+};
+
+struct read_coil_status_q {
+  uint16_t starting_address;
+  uint16_t number_of_points;
+};
+
+struct read_coil_status_r {
+  uint8_t byte_count;
+  uint8_t *data;
+};
+
+#define read_input_status_q read_coil_status_q
+#define read_input_status_r read_coil_status_r
+
+#define read_holding_registers_q read_coil_status_q
+
+struct read_holding_registers_r {
+  uint8_t byte_count;
+  uint16_t *data;
+};
+
+#define read_input_registers_q read_coil_status_q
+#define read_input_registers_r read_holding_registers_r
+
+struct force_single_coil_q {
+  uint16_t address;
+  uint16_t data;
+};
+
+struct force_single_coil_r {
+  uint16_t address;
+  uint16_t data;
+};
+
+#define preset_single_register_q force_single_coil_q
+#define preset_single_register_r force_single_coil_r
+
+struct query {
+  struct query_header header;
+  union {
+    struct read_coil_status_q rcsq;
+    struct force_single_coil_q fscq;
+  };
+};
+
+struct response {
+  struct query_header header;
+  union {
+    struct read_coil_status_r rcsr;
+    struct read_holding_registers_r rhrr;
+    struct force_single_coil_r fscr;
+  };
+};
+
 
 struct emb {
   struct circular_buffer cb;
   uint8_t parse_buffer[STATIC_BUFFER_SIZE];
   unsigned int parse_buffer_offset;
-  void (*process)(const void *data, unsigned int size);
+  uint8_t binary_data[BINARY_BUFFER_SIZE];
+  void (*process_query)(const void *data, unsigned int size, struct query *qs);
+  void (*process_response)(const void *data, unsigned int size, struct response *rs);
   int condition;
+  int master;
 };
 
-int emb_init(struct emb *e, uint8_t *storage, unsigned int storage_size, void (*process)(const void *data, unsigned int size)) {
+
+uint8_t __hex_to_byte(uint8_t hi, uint8_t lo) {
+  uint8_t byte = 0;
+  uint8_t b = hi;
+  if (b>='A' && b<='F') {
+    byte = (uint8_t)(b-'A');
+  } else if (b>='a' && b<='f') {
+    byte = (uint8_t)(b-'a');
+  } else if (b>='0' && b<='9') {
+    byte = (uint8_t)b-'0';
+  }
+  byte<<4;
+  b = lo;
+  if (b>='A' && b<='F') {
+    byte |= (uint8_t)(b-'A');
+  } else if (b>='a' && b<='f') {
+    byte |= (uint8_t)(b-'a');
+  } else if (b>='0' && b<='9') {
+    byte |= (uint8_t)b-'0';
+  }
+  return byte;
+}
+
+int emb_init(struct emb *e, uint8_t *storage, unsigned int storage_size, void (*process_response)(const void *data, unsigned int size, struct response *rs), void (*process_query)(const void *data, unsigned int size, struct query *qs), int master) {
   cb_init(&e->cb, storage, storage_size);
-  e->process = process;
+  e->process_response = process_response;
+  e->process_query = process_query;
   e->condition = COND_START;
   e->parse_buffer_offset = 0;
+  e->master = master;
   return CERR_OK;
+}
+
+int __emb_parse_header(struct emb *e, struct query_header *qh, unsigned int *offset) {
+  int ret = CERR_OK;
+  uint8_t *pb = e->parse_buffer;
+  if (pb[0] == ':') {
+    qh->header = ':';
+  } else {
+    return CERR_ENOTFOUND;
+  }
+  qh->slave_address = __hex_to_byte(pb[1], pb[2]);
+  qh->function = __hex_to_byte(pb[3], pb[4]);
+  *offset = 5;
+  return ret;
+}
+
+int __emb_read_coil_status(struct emb *e, struct read_coil_status_r *rcsr, unsigned int *offset) {
+  int ret = CERR_OK;
+  uint8_t *pb = e->parse_buffer;
+  rcsr->byte_count = __hex_to_byte(pb[*offset], pb[*offset+1]);
+  *offset+=2;
+  unsigned int i = 0;
+  if (rcsr->byte_count > BINARY_BUFFER_SIZE) {
+    return CERR_ENOMEM;
+  }
+  for (;i<rcsr->byte_count;i++) {
+    rcsr->data[i] = __hex_to_byte(pb[*offset], pb[*offset+1]);
+    *offset+=2;
+  }
+  return ret;
 }
 
 int __emb_parse_incoming_data(struct emb *e) {
   printf("processing new data\r\n");
-  e->process(e->parse_buffer, e->parse_buffer_offset);
+  struct query_header qh;
+  unsigned int offset = 0;
+  struct response rs;
+  if (e->master) {
+    // if im master, then responses are incoming
+    __emb_parse_header(e, &qh, &offset);
+    switch (qh.function) {
+    case F_READ_COIL_STATUS:
+    case F_READ_INPUT_STATUS:
+    case F_READ_INPUT_REGISTERS: {
+      memcpy(&(rs.header), &qh, sizeof(struct query_header));
+      rs.rcsr.data = e->binary_data;
+      __emb_read_coil_status(e, &(rs.rcsr), &offset);
+      break;
+    }
+      
+    }
+  }
+  e->process_response(e->parse_buffer, e->parse_buffer_offset, &(rs));
   return CERR_OK;
 }
 
