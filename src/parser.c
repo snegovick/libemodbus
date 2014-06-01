b2fa7572ec4977845908249e32d7af9c396a5652
Kirik snegovick Konstantin <snegovick@gmail.com>
2014-06-01 15:58:29 +0400

    rewrote incoming data parsing, now with pop

--- src/parser.c
+++ src/parser.c
@@ -11,14 +11,29 @@ void process(const void * data, unsigned int size) {
 }
 
 int main (void) {
-  char *buf = ":0603006B0003AB\r\ngjksdfg:0603006b0003AB\r\nsdghsdf\0";
+  char *buf = ":0603006Baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0003AB\r\ngjksdfg:0603006b0003AB\r\nsdghsdf";
   uint8_t storage[32];
   struct emb e;
   emb_init(&e, storage, 32, process);
-  int i = 0;
-  for (;i<49;i++) {
-    printf("in: %c\r\n", buf[i]);
-    emb_process_data(&e, buf+i, 1);
+  int i = 1;
+  char *c = buf;
+  while (1) {
+    printf("in: %c\r\n", *c);
+    emb_push_data(&e, c, 1);
+    i++;
+    if (i%10==0) {
+      if (emb_process_data(&e)!=CERR_OK) {
+        printf ("error\r\n");
+      }
+    }
+    c++;
+    if (*c=='\0') {
+      break;
+    }
   }
+  if (emb_process_data(&e)!=CERR_OK) {
+    printf ("error\r\n");
+  }
+
   return 0;
 }
--- src/parser.h
+++ src/parser.h
@@ -12,47 +12,68 @@
 struct emb {
   struct circular_buffer cb;
   uint8_t parse_buffer[STATIC_BUFFER_SIZE];
+  unsigned int parse_buffer_offset;
   void (*process)(const void *data, unsigned int size);
   int condition;
-  unsigned int start;
-  unsigned int end;
 };
 
 int emb_init(struct emb *e, uint8_t *storage, unsigned int storage_size, void (*process)(const void *data, unsigned int size)) {
   cb_init(&e->cb, storage, storage_size);
   e->process = process;
   e->condition = COND_START;
+  e->parse_buffer_offset = 0;
   return CERR_OK;
 }
 
 int __emb_parse_incoming_data(struct emb *e) {
+  printf("processing new data\r\n");
+  e->process(e->parse_buffer, e->parse_buffer_offset);
+  return CERR_OK;
 }
 
-int emb_process_data(struct emb *e, const uint8_t *new_data, unsigned int data_size) {
+void emb_recover(struct emb *e) {
+  e->condition = COND_START;
+  cb_recover(&(e->cb));
+}
+
+int emb_push_data(struct emb *e, const uint8_t *new_data, unsigned int data_size) {
+  return cb_push(&(e->cb), new_data, data_size);
+}
+
+int emb_process_data(struct emb *e) {
+  int ret = CERR_OK;
   unsigned int i = 0;
-  for (;i<data_size;i++) {
-    printf("pushing %c\r\n", new_data[i]);
-    if ((e->condition == COND_START) && (new_data[i] == ':')) {
+  uint8_t c;
+  while (cb_get_allocated_space(&(e->cb))>0) {
+    cb_pop(&(e->cb), &c, 1);
+    if ((e->condition == COND_START) && (c==':')) {
+      e->parse_buffer_offset = 0;
+      e->parse_buffer[e->parse_buffer_offset] = c;
+      e->parse_buffer_offset ++;
       e->condition = COND_R;
-      e->start = cb_get_current_position(&(e->cb));
-      cb_push_byte(&(e->cb), new_data[i]);
-    } else if ((e->condition == COND_R) && (new_data[i] == '\r')) {
-      cb_push_byte(&(e->cb), new_data[i]);
-      e->condition = COND_N;
-    } else if ((e->condition == COND_N) && (new_data[i] == '\n')) {
-      e->condition = COND_START;
-      cb_push_byte(&(e->cb), new_data[i]);
-      e->end = cb_get_current_position(&e->cb);
-
-      unsigned int acq_size = 0;
-      if (cb_get_data(&e->cb, e->start, e->end, e->parse_buffer, STATIC_BUFFER_SIZE, &acq_size)==CERR_OK) {
-        e->process(e->parse_buffer, acq_size);
+    } else if (e->condition == COND_R) {
+      e->parse_buffer[e->parse_buffer_offset] = c;
+      e->parse_buffer_offset ++;
+      if (c=='\r') {
+        e->condition = COND_N;
       }
+    } else if ((e->condition == COND_N)) {
+      if (c != '\n') {
+        emb_recover(e);
+      } else {
+        e->parse_buffer[e->parse_buffer_offset] = c;
+        e->parse_buffer_offset ++;
+        ret = __emb_parse_incoming_data(e);
+        e->condition = COND_START;
+      }
+    }
 
-    } else {
-      cb_push_byte(&(e->cb), new_data[i]);
+    if (e->parse_buffer_offset >= STATIC_BUFFER_SIZE) {
+      e->condition = COND_START;
+      return CERR_ENOMEM;
     }
   }
+  return ret;
 }
 
 #endif/*__PARSER_H__*/
